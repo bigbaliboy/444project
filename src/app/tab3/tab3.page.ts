d150f363463d2dbdeb3bcb2bb76d950920f84475
bigbaliboy <59143677+bigbaliboy@users.noreply.github.com>
2022-11-24 14:18:51 +0300

    Initial
    initial commit  for the application

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+
+export class Tab3Page {
+   isEdit:boolean;
+   isSave:boolean = false;
+  constructor() {
+    this.isEdit=true
+  }
+
+  Edit(){
+    this.isEdit=false
+    this.isSave=true
+  }
+  Save(){
+    this.isEdit=true
+    this.isSave=false
+  }
+}
