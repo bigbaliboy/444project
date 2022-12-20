bf23305c465b44e37125499d74dda04ab1aa9c04
salman-manaa <salman.h.manaa@gmail.com>
2022-12-20 17:54:14 +0300

    salman file update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+
 export class Tab2Page {
 
-  constructor() {}
+  today:any;
 
-}
+  
+
+  constructor() {
+    
+    this.today = Date.now();
+    
+  }
+  }
